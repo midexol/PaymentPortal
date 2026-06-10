ae5a1d079610ef2bf6dc7ee90245ea1046005a20
Mide_xol <okunolaolamide7@gmail.com>
2026-06-10 14:40:44 +0100

    feat: Added vercel config and monorepo build script

--- payment-portal/app.js
+++ payment-portal/app.js
@@ -71,6 +71,8 @@ mongoose
     console.error('❌ MongoDB connection failed:', err.message);
   });
 
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+}
 
 module.exports = app;
